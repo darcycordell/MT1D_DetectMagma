0fd436eb97079c186e7c57a72b7cfa22cbe3072b
darcycordell <dcordell@ualberta.ca>
2022-02-05 12:44:05 -0700

    MAL
    Solved for phi algebraically!

--- MAL3.m
+++ MAL3.m
@@ -1,4 +1,4 @@
-function [sig] = MAL3(sigc,sigm,sigg,phic,phim,phig)
+function [sig] = MAL3(sigc,sigm,sigg,phic,phim,phig,mm,mg)
 % Solve Modified Archie's Law (Glover et al., 2000)
 %
 % Usage: sig = archie(phi, sigm, sigf, m)
@@ -10,25 +10,37 @@ function [sig] = MAL3(sigc,sigm,sigg,phic,phim,phig)
 %       m = connectivity parameters
 %
 %
-m = 1;
+if phic == 0
+    phic = 10^-4;
+end
 
-Gc = phic.^m;
-Gm = phim.^m;
-Gg = phig.^m;
+Gm = phim.^mm;
+Gg = phig.^mg;
+
+mc = log(1-Gg-Gm)./log(phic);
+
+Gc = phic.^mc;
 
 Gchecksum = Gc + Gm + Gg;
-
-phichecksum = phic + phim + phig;
 
 sig = sigc.*Gc + sigm.*Gm + sigg.*Gg;
 
 if abs(Gchecksum-1) > 10^-3
-    disp('G not equal 1')
+   error('G not equal 1. Something wrong.')
 end
 
-if abs(phichecksum-1) > 10^-3
-    disp('phi not equal 1')
+if imag(1/sig)>10^-3
+    error('Imaginary conductivies. Something wrong.')
+else
+    sig = real(sig);
 end
+    
+
+%     phichecksum = phic + phim + phig;
+%     if abs(phichecksum-1) > 10^-3
+%         disp('phi not equal 1')
+%     end
+
 
 
 
--- MAL_solve_phi.m
+++ MAL_solve_phi.m
@@ -40,52 +40,59 @@ if rho_f > rho_h
     flag = 1;
 end
 
+i = NaN;
 
-N_max = 1000; %Maximum # of iterations (rarely need >10)
-epsilon = 10^-5; %Stopping criterion (rarely need melt fractions with greater precision than this)
-x = zeros(N_max,1); %Initialize unknown
-x(1) = 0.25; %Starting guess is 0.5 just because 0 < phi < 1
+sigb = 1./rho_b;
+sigf = 1./rho_f;
+sigh = 1./rho_h;
 
-i = 2;
-while 1   
-    p = log10(1-x(i-1).^m)./log10(1-x(i-1)); %set p
-    f = rho_f*rho_h*(rho_h*x(i-1).^m+rho_f*(1-x(i-1)).^p).^-1-rho_b; %MAL
-    %Derivate of f is ridiculously complicated because of the "p" (e.g. phi to an
-    %exponent containing phi).
-    da = m*rho_h*x(i-1).^(m-1);
-    db = rho_f*(1-x(i-1)).^p.*((log10(rho_f)*log10(1-x(i-1).^m).^2-m*x(i-1).^(m-1).*log10(rho_f).*log10(1-x(i-1)).^2-m*x(i-1).^(m-1).*log10(1-x(i-1)).^3)./(log10(1-x(i-1).^m).*log10(1-x(i-1)).^3));
-    fp = -rho_f*rho_h*(rho_h*x(i-1).^m+rho_f*(1-x(i-1)).^p).^-2.*(da+db);
+phi = ((sigb-sigh)./(sigf-sigh)).^(1/m);
 
-
-    x(i) = x(i-1) - f/fp; %Newton-Raphson
-    
-    if x(i)>=1 %If the algorithm finds a resistivity >1 then this is non-physical so maybe try a different starting guess
-        x(i) = 0.99;
-    end
-    
-    if x(i)<=0 %Similarly if the algorithm finds a resistivity <0 then try a different starting guess
-        x(i) = 0.01;
-    end
-
-    %Stopping criteria
-    if abs(x(i)-x(i-1))<epsilon
-        break
-    end
-
-    if i>1000
-        break
-    end
-
-    %x(i)
-    
-    i = i+1;
-    
-    
-
-end
-
-phi = x(i);
+% N_max = 1000; %Maximum # of iterations (rarely need >10)
+% epsilon = 10^-5; %Stopping criterion (rarely need melt fractions with greater precision than this)
+% x = zeros(N_max,1); %Initialize unknown
+% x(1) = 0.5; %Starting guess is 0.5 just because 0 < phi < 1
+% 
+% i = 2;
+% while 1   
+%     p = log10(1-x(i-1).^m)./log10(1-x(i-1)); %set p
+%     f = rho_f*rho_h*(rho_h*x(i-1).^m+rho_f*(1-x(i-1)).^p).^-1-rho_b; %MAL
+%     %Derivate of f is ridiculously complicated because of the "p" (e.g. phi to an
+%     %exponent containing phi).
+%     da = m*rho_h*x(i-1).^(m-1);
+%     db = rho_f*(1-x(i-1)).^p.*((log10(rho_f)*log10(1-x(i-1).^m).^2-m*x(i-1).^(m-1).*log10(rho_f).*log10(1-x(i-1)).^2-m*x(i-1).^(m-1).*log10(1-x(i-1)).^3)./(log10(1-x(i-1).^m).*log10(1-x(i-1)).^3));
+%     fp = -rho_f*rho_h*(rho_h*x(i-1).^m+rho_f*(1-x(i-1)).^p).^-2.*(da+db);
+% 
+% 
+%     x(i) = x(i-1) - f/fp; %Newton-Raphson
+%     
+%     if x(i)>=1 %If the algorithm finds a resistivity >1 then this is non-physical so maybe try a different starting guess
+%         x(i) = 0.99;
+%     end
+%     
+%     if x(i)<=0 %Similarly if the algorithm finds a resistivity <0 then try a different starting guess
+%         x(i) = 0.01;
+%     end
+% 
+%     %Stopping criteria
+%     if abs(x(i)-x(i-1))<epsilon
+%         break
+%     end
+% 
+%     if i>1000
+%         break
+%     end
+% 
+%     %x(i)
+%     
+%     i = i+1;
+%     
+%     
+% 
+% end
+% 
+% phi = x(i);
 
 if flag == 1
-    phi = NaN; i = NaN;
+    phi = 1; i = NaN;
 end
